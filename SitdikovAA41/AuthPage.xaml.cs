2c1563b0cba7b3ea6d981b10bfcba9dbed91c125
Элиза Ситдикова <elizasitdikova904@gmail.com>
2025-03-04 11:20:31 +0500

    Добавьте файлы проекта.

---
+++ SitdikovAA41/AuthPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SitdikovAA41
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = log_TextChanged;
+            string password = par_TextChanged;
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Есть пустые поля");
+                return;
+            }
+
+            User user = Sitdi41Entities.GetContext().User.ToList().Find(p => p.UserLogin == login && p.UserPassword == password);
+            if (user != null)
+            {
+                Manager.MainFrame.Navigate(new ProductPage(user));
+                log_TextChanged = "";
+                par_TextChanged = "";
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные");
+                loginBtn.IsEnabled = false;
+                loginBtn.IsEnabled = true;
+            }
+        }
+
+        private void log_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            string username = log.Text; // Получаем текст из TextBox
+
+            // Проверяем, не пустой ли логин
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                bool isValid = CheckUsernameInDatabase(username); // Проверяем наличие логина в базе данных
+                loginBtn.IsEnabled = isValid; // Включаем или отключаем кнопку входа
+            }
+            else
+            {
+                loginBtn.IsEnabled = false; // Если поле пустое, отключаем кнопку
+            }
+        }
+
+        private bool CheckUsernameInDatabase(string username)
+        {
+            using (var context = new Sitdi41Entities()) // Создаем экземпляр контекста базы данных
+            {
+                // Проверяем, существует ли пользователь с таким логином
+                return context.User.Any(u => u.Username == username);
+            }
+        }
+    }
+        
+
+        private void par_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SitdikovAA41/ProductPage.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SitdikovAA41
+{
+
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        int CountRecords;//Количество записей в таблице
+        int CountPage;//Общее количество страниц
+        int CurrentPage = 0;//Текущая страница
+
+        List<Product> CurrentPageList = new List<Product>();
+        List<Product> TableList;
+        public ProductPage()
+        {
+            InitializeComponent();
+
+            var currentProduct = Sitdi41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
+            UpdateProduct();
+            ComboType.SelectedIndex = 0;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+        //private void ChangePage(int direction, int? selectedPage)
+        //{
+        //    CurrentPageList.Clear();
+        //    CountRecords = TableList.Count;
+
+        //    if (CountRecords % 10 > 0)
+        //    {
+        //        CountPage = CountRecords / 10 + 1;
+        //    }
+        //    else
+        //    {
+        //        CountPage = CountRecords / 10;
+        //    }
+
+        //    Boolean Ifupdate = true;
+
+        //    int min;
+
+        //    if (selectedPage.HasValue)
+        //    {
+        //        if (selectedPage >= 0 && selectedPage <= CountPage)
+        //        {
+        //            CurrentPage = (int)selectedPage;
+        //            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+        //            for (int i = CurrentPage * 10; i < min; i++)
+        //            {
+        //                CurrentPageList.Add(TableList[i]);
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        switch (direction)
+        //        {
+        //            case 1:
+        //                if (CurrentPage > 0)
+        //                {
+        //                    CurrentPage--;
+        //                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+        //                    for (int i = CurrentPage * 10; i < min; i++)
+        //                    {
+        //                        CurrentPageList.Add(TableList[i]);
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    Ifupdate = false;
+        //                }
+        //                break;
+
+        //            case 2:
+        //                if (CurrentPage < CountPage - 1)
+        //                {
+        //                    CurrentPage++;
+        //                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+        //                    for (int i = CurrentPage * 10; i < min; i++)
+        //                    {
+        //                        CurrentPageList.Add(TableList[i]);
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    Ifupdate = false;
+        //                }
+        //                break;
+        //        }
+        //    }
+        //    if (Ifupdate)
+        //    {
+        //        PageListBox.Items.Clear();
+
+        //        for (int i = 1; i <= CountPage; i++)
+        //        {
+        //            PageListBox.Items.Add(i);
+        //        }
+        //        PageListBox.SelectedIndex = CurrentPage;
+
+        //        min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+
+        //        //Это вывод надписи 10 из 100 и тд
+        //        TBCount.Text = min.ToString();
+        //        TBAllRecords.Text = " из " + CountRecords.ToString();
+
+        //        ProductListView.ItemsSource = CurrentPageList;
+        //        ProductListView.Items.Refresh();
+        //    }
+        //}
+
+        private void UpdateProduct()
+        {
+            var currentServices = Sitdi41Entities.GetContext().Product.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 9.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 14.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) < 100)).ToList();
+            }
+
+
+            currentServices = currentServices.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            ProductListView.ItemsSource = currentServices.ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderBy(p => p.ProductCost).ToList();
+
+            }
+            ProductListView.ItemsSource = currentServices;
+            TableList = currentServices;
+            счёт.Text = "Количество " + currentServices.Count.ToString() + " из " + Sitdi41Entities.GetContext().Product.ToList().Count.ToString();
+        }
+
+    
+
+
+        private void ComboType_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+
+        //private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+
+        //}
+
+    }
+}
+
+
